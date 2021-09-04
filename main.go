7814592cb3285a87b906a7e98dbbf09402c71ffb
weisuoyuwei159 <69943766+weisuoyuwei159@users.noreply.github.com>
2021-09-04 21:41:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func main() {
 		// SuperUsers 某些功能需要主人权限，可通过以下两种方式修改
 		// []string{}：通过代码写死的方式添加主人账号
 		// os.Args[1:]：通过命令行参数的方式添加主人账号
-		SuperUsers: append([]string{"12345678", "87654321"}, os.Args[1:]...),
+		SuperUsers: append([]string{"3159258134", "87654321"}, os.Args[1:]...),
 
 		Driver: []zero.Driver{
 			&driver.WSClient{
